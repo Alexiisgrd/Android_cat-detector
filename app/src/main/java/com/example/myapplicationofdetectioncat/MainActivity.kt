661f8b40fbf207dc66f322c288be82052de6b9d7
Alexis GAIRAUD <alexisgairaud@gmail.com>
2025-02-22 18:16:21 +0100

    Add Vibration

--- app/src/main/java/com/example/myapplicationofdetectioncat/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationofdetectioncat/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplicationofdetectioncat
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     private var httpServer: MyHttpServer? = null
     private lateinit var statusTextView: TextView
     private lateinit var statusTextViewStatusChat: TextView
-
     private lateinit var controlButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,12 +47,32 @@ class MainActivity : AppCompatActivity() {
         controlButton.text = if (isRunning) "Arrêter le serveur" else "Démarrer le serveur"
     }
 
+    private fun vibratePhone() {
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+
+        if (vibrator.hasVibrator()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(2000, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                @Suppress("DEPRECATION")
+                vibrator.vibrate(2000) // Vibrer pendant 500 millisecondes
+            }
+        }
+    }
+
     inner class MyHttpServer(private val port: Int) : NanoHTTPD(port) {
 
         override fun serve(session: IHTTPSession): Response {
             if (session.method == Method.POST && session.uri == "/detect") {
                 runOnUiThread {
                     statusTextViewStatusChat.text = "Chat détecté !"
+                    vibratePhone() // Faire vibrer le téléphone
                 }
                 return newFixedLengthResponse("Chat detected!")
             }
